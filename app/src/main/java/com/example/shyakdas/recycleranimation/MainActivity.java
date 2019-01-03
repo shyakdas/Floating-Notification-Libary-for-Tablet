c916edfdd825adf2301012023cc0285265158927
Shyak Das <shayak.das007@gmail.com>
2019-01-03 21:45:51 +0530

    Unecessary import removed

--- app/src/main/java/com/example/shyakdas/recycleranimation/MainActivity.java
+++ app/src/main/java/com/example/shyakdas/recycleranimation/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.floatingnotification.FloatNotificationViewModel;
-import com.example.floatingnotification.models.MessageParser;
 import com.example.floatingnotification.models.NotificationItem;
 
 import java.util.ArrayList;
